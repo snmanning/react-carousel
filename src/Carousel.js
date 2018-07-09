af22cefc78ca417810bdcb4b29a5fee7d4289cb1
Sarah Manning <sarahnmanning@gmail.com>
2018-07-09 18:56:10 -0400

    set up component , images won't load.

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,34 +1,64 @@
 import React, { Component } from 'react';
 import './Carousel.css';
+import Buddy from './images/Buddy.jpg';
+import Cuties from './images/Cuties.jpg';
+import Sassy from '.images/Sassy.jpg';
+import Max_and_ThePrince from './images/Max_and_ThePrince';
+
+console.log(Buddy)
+console.log(Cuties)
+console.log(Sassy)
+console.log(Max_and_ThePrince)
 
 class Carousel extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        viewer: ''
-      }
+        viewer: '',
+      };
     }
 
-  changePic(){
+  changePic(image){
     this.setState({
-      viewer: 
+      viewer: image
     });
   }
 
+  slidePicR(c) {
+    this.setState({
+      left: c.target.value
+    })
+  }
+
+  slidePicL(c) {
+    this.setState({
+      right: c.target.value
+    })
+  }
+
   render() {
+    const style = {
+        slideStyle: {
+          viewer: this.state.viewer,
+        }
+    };
+    const {slideStyle} = style;
     return (
       <div className='Carousel-window'>
         {this.props.title ? <h1 className='Carousel-title'>{this.props.title}</h1> : null}
-          <div className='Carousel-slide'>
-            <img className='Carousel-images' src='./images/Buddy.jpg' alt='fat orange cat'/>
-            <img className='Carousel-images' src='./images/Cuties.jpg' alt='orange kittens in box'/>
-            <img className='Carousel-images' src='./images/Sassy.jpg' alt='cat beside coffee maker'/>
-            <img className='Carousel-images' src='./images/Max_and_ThePrince' alt='black dog and silver cat playing'/>
+          <div className='Carousel-slide' style={slideStyle}>{this.state.viewer}
+            <img className='Carousel-images' src={Buddy} alt='fat orange cat'/>
+            <img className='Carousel-images' src={Cuties} alt='orange kittens in box'/>
+            <img className='Carousel-images' src={Sassy} alt='cat beside coffee maker'/>
+            <img className='Carousel-images' src={Max_and_ThePrince} alt='black dog and silver cat playing'/>
           </div>
           <section className='Carousel-controls'>
             <button className='Carousel-move' 
-                    onClick/>
-            <button className='Carousel-move' />
+                    onClick={() => this.changePic(this.state.left)}>
+                    Right </button>
+            <button className='Carousel-move' 
+                    onClick={() => this.changePic(this.state.right)} >
+                    Left </button>
           </section>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import Carousel from './Carousel';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Carousel />, document.getElementById('root'));
 registerServiceWorker();
