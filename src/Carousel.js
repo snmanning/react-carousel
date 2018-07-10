5baea97869ac53acc062d2ff270980379deef840
Sarah Manning <sarahnmanning@gmail.com>
2018-07-09 20:54:40 -0400

    renamed imgs camelCase. added an array in the state. modified css so I could see changes

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,45 +1,47 @@
 import React, { Component } from 'react';
 import './Carousel.css';
-import Buddy from './images/Buddy.jpg';
-import Cuties from './images/Cuties.jpg';
-import Sassy from '.images/Sassy.jpg';
-import Max_and_ThePrince from './images/Max_and_ThePrince';
+import buddy from './images/buddy.jpg';
+import cuties from './images/cuties.jpg';
+import sassy from './images/sassy.jpg';
+import maxAndThePrince from './images/maxAndThePrince.jpg';
 
-console.log(Buddy)
-console.log(Cuties)
-console.log(Sassy)
-console.log(Max_and_ThePrince)
 
 class Carousel extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        viewer: '',
+        viewerImages: [
+          './images/buddy.jpg',
+          './images/cuties.jpg',
+          './images/sassy.jpg',
+          './images/maxAndThePrince.jpg'
+        ],
+        viewerIndex: 0,
       };
     }
 
   changePic(image){
     this.setState({
-      viewer: image
+      viewerImages: image
     });
   }
 
-  slidePicR(c) {
-    this.setState({
-      left: c.target.value
-    })
-  }
+  // slidePicR(c) {
+  //   this.setState({
+  //     left: c.target.value
+  //   })
+  // }
 
-  slidePicL(c) {
-    this.setState({
-      right: c.target.value
-    })
-  }
+  // slidePicL(c) {
+  //   this.setState({
+  //     right: c.target.value
+  //   })
+  // }
 
   render() {
     const style = {
         slideStyle: {
-          viewer: this.state.viewer,
+          viewer: this.state.viewerImages,
         }
     };
     const {slideStyle} = style;
@@ -47,17 +49,17 @@ class Carousel extends Component {
       <div className='Carousel-window'>
         {this.props.title ? <h1 className='Carousel-title'>{this.props.title}</h1> : null}
           <div className='Carousel-slide' style={slideStyle}>{this.state.viewer}
-            <img className='Carousel-images' src={Buddy} alt='fat orange cat'/>
-            <img className='Carousel-images' src={Cuties} alt='orange kittens in box'/>
-            <img className='Carousel-images' src={Sassy} alt='cat beside coffee maker'/>
-            <img className='Carousel-images' src={Max_and_ThePrince} alt='black dog and silver cat playing'/>
+            <img className='Carousel-images' src={buddy} alt='fat orange cat'/>
+            <img className='Carousel-images' src={cuties} alt='orange kittens in box'/>
+            <img className='Carousel-images' src={sassy} alt='cat beside coffee maker'/>
+            <img className='Carousel-images' src={maxAndThePrince} alt='black dog and silver cat playing'/>
           </div>
           <section className='Carousel-controls'>
             <button className='Carousel-move' 
-                    onClick={() => this.changePic(this.state.left)}>
+                    onClick={() => this.changePic(this.state.viewer-1)}>
                     Right </button>
             <button className='Carousel-move' 
-                    onClick={() => this.changePic(this.state.right)} >
+                    onClick={() => this.changePic(this.state.viewer+1)} >
                     Left </button>
           </section>
       </div>
